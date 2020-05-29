dbed1ca2cfddc8eb00b47df58b430146054028f7
alaghusundhar <alaghusundhar@hotmail.com>
2020-05-28 22:08:37 -0500

    Leet Code Solution

---
+++ target_array.py
@@ -0,0 +1,35 @@
+""" Author : Alagusundaram Nithyanandam | Leet Code Solution : Create Target Array in the Given Order
+Given two arrays of integers nums and index. Your task is to create target array under the following rules:
+
+Initially target array is empty.
+From left to right read nums[i] and index[i], insert at index index[i] the value nums[i] in target array.
+Repeat the previous step until there are no elements to read in nums and index.
+Return the target array.
+
+It is guaranteed that the insertion operations will be valid.
+eg Input: nums = [0,1,2,3,4], index = [0,1,2,2,1]
+Output: [0,4,1,3,2]
+Explanation:
+nums       index     target
+0            0        [0]
+1            1        [0,1]
+2            2        [0,1,2]
+3            2        [0,1,3,2]
+4            1        [0,4,1,3,2]
+
+"""
+from typing import List
+
+nums = [0,1,2,3,4]
+index = [0,1,2,2,1]
+
+class Solution:
+    def createTargetArray(self, nums: List[int], index: List[int]) -> List[int]:
+        target=[]
+        for i,j in zip(nums,index):
+            target.insert(j,i)
+        return target
+
+
+instace_of_solution = Solution()
+print(instace_of_solution.createTargetArray(nums,index))
